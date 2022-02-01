63ba5cea693769ee282bd295e0176c68d2f73382
Sarah Butlin <91489047+DragonKnight240@users.noreply.github.com>
2022-02-01 23:19:37 +0000

    Using new camera controls
    The camera works as intended in the prototype scene

--- Lunar Panda Game/Assets/Programming/Izaak/lockMouse.cs
+++ Lunar Panda Game/Assets/Programming/Izaak/lockMouse.cs
@@ -18,13 +18,18 @@ public class lockMouse : MonoBehaviour
     public float cameraLowerBoundsX;
     public float cameraUpperBoundsY;
     public float cameraLowerBoundsY;
+
+    public bool canLook;
     
     void Start()
     {
+        canLook = true;
     }
 
     void Update()
     {
+        if (canLook)
+        {
             Cursor.lockState = CursorLockMode.Locked;
             mouseX = Input.GetAxis("Mouse X") * cameraRotateSpeed;
             mouseY = Input.GetAxis("Mouse Y") * cameraRotateSpeed;
@@ -32,7 +37,8 @@ public class lockMouse : MonoBehaviour
             xRotation -= mouseY;
             xRotation = Mathf.Clamp(xRotation, cameraLowerBoundsX, cameraUpperBoundsX);
 
-        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
-        playerObj.Rotate(Vector3.up * mouseX);
+            transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+            playerObj.Rotate(Vector3.up * mouseX);
+        }
     }
  }
