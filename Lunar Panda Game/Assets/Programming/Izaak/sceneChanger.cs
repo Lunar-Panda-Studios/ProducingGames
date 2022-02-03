63cfd626ab6d11c8ff0e05fdda5c381b89fa748c
Unovi93 <98537015+Unovi93@users.noreply.github.com>
2022-02-03 19:00:28 +0000

    Test Scenes for Typewriter
    ^^

---
+++ Lunar Panda Game/Assets/Programming/Izaak/sceneChanger.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneChanger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Jump"))
+        {
+            SceneManager.LoadScene("Test Scene 2");
+        }
+    }
+}
