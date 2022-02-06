fdfebd8973eecf8596cc74ed5ab8201cac607b6f
Carabu <connorregan25@gmail.com>
2022-02-06 13:38:38 +0000

    Stam bar and opening/closing
    stam bar is basic and doesnt effect movement its just a simple bar that gets used and regenerates.
    
    opening and closing extremely basic and needs camerons code to be added in.

---
+++ Lunar Panda Game/Assets/Programming/Connor/Scripts/StaminaBar.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StaminaBar : MonoBehaviour
+{
+    public Slider staminaBar;
+    [Tooltip("maximum stamina value")]
+    public int maxStam = 100;
+    public int currentStam;
+    [Tooltip("delay before player starts regaining stamina")]
+    public int regenDelay = 1;
+    [Tooltip("higher the number the smaller the regeneration")]
+    public int regenAmount = 100;
+
+    public static StaminaBar instance;
+
+    private WaitForSeconds regenPerFrame = new WaitForSeconds(0.1f);
+    private Coroutine regenCr; 
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+        currentStam = maxStam;
+        staminaBar.maxValue = maxStam;
+        staminaBar.value = maxStam;
+    }
+
+    public void staminaUsage(int amount)
+    {
+        if(currentStam - amount >= 0) // checks to see if you have enough stamina to perform action
+        {
+            currentStam -= amount;
+            staminaBar.value = currentStam;
+
+            if(regenCr != null)
+            {
+                StopCoroutine(regenCr);
+            }
+
+            regenCr = StartCoroutine(stamRegen());
+        }
+    }
+
+    private IEnumerator stamRegen()
+    {
+        yield return new WaitForSeconds(regenDelay);  
+
+        while(currentStam < maxStam)
+        {
+            currentStam += maxStam / regenAmount;
+            staminaBar.value = currentStam;
+
+            yield return regenPerFrame;
+        }
+        regenCr = null;
+    }
+
+}
