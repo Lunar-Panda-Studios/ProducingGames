b2ff7ba7b9da41075dc32636850c05fe05533e93
Carabu <connorregan25@gmail.com>
2022-02-08 22:05:53 +0000

    quick fix for prototype
    stamina to revisit later

--- Lunar Panda Game/Assets/Programming/Izaak/playerMovement.cs
+++ Lunar Panda Game/Assets/Programming/Izaak/playerMovement.cs
@@ -8,10 +8,7 @@ public class playerMovement : MonoBehaviour
 
     [Header("Move Settings")]
     [Tooltip("Speed the player moves at")]
-    public float stamSpeed = 12;
     public float p_speed = 5;
-    public float slowSpeed = 1;
-    public float lowStamThreshold = 25;
     public float runStamReq = 0.02f;
 
     void Start()
@@ -24,46 +21,32 @@ public class playerMovement : MonoBehaviour
 
     void Update()
     {
+        //Collects the horizontal and forward inputs for this frame in variables
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
-        //Collects the horizontal and forward inputs for this frame in variables
-        //Sets the velocity based on these values to move the player
-        if ((runStamReq > StaminaBar.instance.currentStam) || StaminaBar.instance.currentStam < lowStamThreshold)
+        //if (StaminaBar.instance.currentStam <= 10)
+        //{
+        //    p_speed = 2.0f;
+        //}
+        if (Input.GetKey(KeyCode.LeftShift) && (StaminaBar.instance.currentStam > runStamReq))
         {
-            moveSlow();
-        }
-        else if(Input.GetKey(KeyCode.LeftShift))
-        {
-            moveFast();
-        }
-        else
-        {
-            move();
-        }
-
-        Debug.Log(p_rigidbody.velocity.x);
-        Debug.Log(p_rigidbody.velocity.y);
-    }
-        void move()
-        {
-            float x = Input.GetAxis("Horizontal");
-            float z = Input.GetAxis("Vertical");
-            p_rigidbody.velocity = ((transform.forward * z) * p_speed) + ((transform.right * x) * p_speed) + (new Vector3(0, p_rigidbody.velocity.y, 0));
-        }
-
-        void moveFast()
-        {
-            float x = Input.GetAxis("Horizontal");
-            float z = Input.GetAxis("Vertical");
-            p_rigidbody.velocity = ((transform.forward * z) * stamSpeed) + ((transform.right * x) * stamSpeed) + (new Vector3(0, p_rigidbody.velocity.y, 0));
+            p_speed = 10.0f;
             StaminaBar.instance.staminaUsage(runStamReq);
         }
-
-        void moveSlow()
+        else if (true)
         {
-            float x = Input.GetAxis("Horizontal");
-            float z = Input.GetAxis("Vertical");
-            p_rigidbody.velocity = ((transform.forward * z) * slowSpeed) + ((transform.right * x) * slowSpeed) + (new Vector3(0, p_rigidbody.velocity.y, 0));
+            p_speed = 5.0f;        
         }
+
+        move();
+
+    }
+    void move()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        //Sets the velocity based on these values to move the player
+        p_rigidbody.velocity = ((transform.forward * z) * p_speed) + ((transform.right * x) * p_speed) + (new Vector3(0, p_rigidbody.velocity.y, 0));
+    }
 }
