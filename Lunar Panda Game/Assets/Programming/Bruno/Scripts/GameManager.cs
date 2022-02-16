fe3e45cf3bb5a74e7cb6d376c5a60468c16fa3e5
Cerque <91639254+Cerque@users.noreply.github.com>
2022-02-16 17:03:04 +0000

    Game Manager Start

--- Lunar Panda Game/Assets/Programming/Bruno/Scripts/GameManager.cs
+++ Lunar Panda Game/Assets/Programming/Bruno/Scripts/GameManager.cs
@@ -1,9 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+
+
+
 
 public class GameManager : MonoBehaviour
 {
+
+    // Load Scene
+    
+    public enum Scenes
+    {
+        MENU, GAME, PAUSE, GAMEOVER, QUIT
+    }
+
+    public static GameManager Instance;
+
+    public static void Load(Scenes scene) 
+    {
+    
+    }
+    
     // Start is called before the first frame update
     void Start()
     {
