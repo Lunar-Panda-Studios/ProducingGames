3c493e2a716bfa036090cb816a220a52cd860f1f
Cerque <91639254+Cerque@users.noreply.github.com>
2022-02-22 02:36:25 +0000

    Added Tooltips and comments in the script

--- Lunar Panda Game/Assets/Teams/Programming/Bruno/Scripts/Destructable.cs
+++ Lunar Panda Game/Assets/Teams/Programming/Bruno/Scripts/Destructable.cs
@@ -4,19 +4,26 @@ using UnityEngine;
 
 public class Destructable : MonoBehaviour
 {
-    public GameObject destroyedVersion;      
 
+    [Header("Prefabs")]
+    [Tooltip("Destroyable Version of the gameobject")]
+    public GameObject destroyedVersion;
+
+    [Header("Misc")]
+    [Tooltip("Hammer's item data")]
     public ItemData Hammer;
+    [Tooltip("Inventory's script")]
     public Inventory inventoryScript;
 
     InteractRaycasting raycast;
     private void Start()
     {
-        raycast = FindObjectOfType<InteractRaycasting>();
+        raycast = FindObjectOfType<InteractRaycasting>(); // Needed to stop the object rotating from activating the script as well
     }
 
     void Update()
     {
+        // Using left mouse button as the interactable key
         if (Input.GetButtonDown("Fire1"))
         {
             destroyObject();
@@ -28,12 +35,15 @@ public class Destructable : MonoBehaviour
         RaycastHit hit;
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
+        //Currently using Connor's raycast script
+
         if (raycast.raycastInteract(out hit))
         {
             if (hit.transform.gameObject == gameObject)
             {
                 if (inventoryScript.itemInventory[inventoryScript.selectedItem] == Hammer)
                 {
+                    //It instantiates the destroyable version of the game object in the same position as the original object and destroys the original object
                     Instantiate(destroyedVersion, transform.position, transform.rotation);
                     Destroy(gameObject);
                 }
