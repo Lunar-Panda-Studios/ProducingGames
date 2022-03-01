0e2683f62d0c08ec0dd4ffcbc5ea60f10edc317f
Carabu <connorregan25@gmail.com>
2022-03-01 12:03:55 +0000

    vsync script
    123

--- Lunar Panda Game/Assets/Teams/Programming/Connor/Scripts/VSync.cs
+++ Lunar Panda Game/Assets/Teams/Programming/Connor/Scripts/VSync.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class VSync : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        //need fps counter to test, also test with this vs in proj settings. drag onto empty game obj
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        QualitySettings.vSyncCount = 1;
+        //QualitySettings.vSyncCount = 2;
+        //QualitySettings.vSyncCount = 3;
+        //QualitySettings.vSyncCount = 4;
+        //QualitySettings.vSyncCount = 0;
     }
 }
