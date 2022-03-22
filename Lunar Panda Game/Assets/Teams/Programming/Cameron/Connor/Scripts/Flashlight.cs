7239d0b0fbf7d55f7d593881b1d8aff8e1f5c798
Filip J <5jozif81@solent.ac.uk>
2022-03-22 18:25:20 +0000

    scary stuff
    Flashlight fix
    Fog adjustment
    Adding corpses

--- Lunar Panda Game/Assets/Teams/Programming/Cameron/Connor/Scripts/Flashlight.cs
+++ Lunar Panda Game/Assets/Teams/Programming/Cameron/Connor/Scripts/Flashlight.cs
@@ -15,14 +15,14 @@ public class Flashlight : MonoBehaviour
 
 
     //Matej changes - just flickering :)
-    public float minWaitTime;
-    public float maxWaitTime;
+    public float minWaitTime = 5;
+    public float maxWaitTime = 8;
 
-    public float stopMinWaitTime;
-    public float stopMaxWaitTime;
+    public float stopMinWaitTime = 0;
+    public float stopMaxWaitTime = 0.2f;
 
-    public int minFlicks;
-    public int maxFlicks;
+    public int minFlicks = 2;
+    public int maxFlicks = 3;
 
 
     private bool isOn;
