62fff1d16d545a1a2fd31bb28253241dd4f8fd71
Sharpy <cam-chester@hotmail.co.uk>
2022-03-22 20:28:02 +0000

    couple changes to loading

--- Lunar Panda Game/Assets/Teams/Programming/Cameron/Scripts/LevelManager.cs
+++ Lunar Panda Game/Assets/Teams/Programming/Cameron/Scripts/LevelManager.cs
@@ -2,23 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using System.Threading.Tasks;
+using UnityEngine.UI;
 
 public class LevelManager : MonoBehaviour
 {
+    [SerializeField] Image fadePanel;
+    [SerializeField] float fadeTime;
+    float currentFadeTime;
+
     void Awake()
     {
-        LoadConnectorScene();
+        StartCoroutine(LoadConnectorScene());
         DontDestroyOnLoad(gameObject);
         
     }
 
-    async void LoadConnectorScene()
+    IEnumerator LoadConnectorScene()
     {
         AsyncOperation load = SceneManager.LoadSceneAsync("Connect", LoadSceneMode.Additive);
         while (!load.isDone)
         {
-            await Task.Yield();
+            yield return null;
         }
+    }
+
+    IEnumerator FadeLoadingScreen()
+    {
+        fadePanel.color = new Color(0, 0, 0, currentFadeTime);
+        while (currentFadeTime < fadeTime)
+        {
+            fadeTime += Time.deltaTime;
+            yield return null;
+        }
+        //load shit here
+
+        //if shit loaded
+        //fade loading screen out
+        //idk if that should all be in this single function, but i dont care
     }
 }
