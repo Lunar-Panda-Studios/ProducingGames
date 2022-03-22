3487a8fbffe6fadedd503ddc6f1984f7762192ea
gajdm <99370045+gajdm@users.noreply.github.com>
2022-03-22 22:09:39 +0000

    Adding Sounds

--- Lunar Panda Game/Assets/Teams/Design Team/Game Design/Puzzles Design/Matej/Scripts/FallObject.cs
+++ Lunar Panda Game/Assets/Teams/Design Team/Game Design/Puzzles Design/Matej/Scripts/FallObject.cs
@@ -7,35 +7,42 @@ public class FallObject : MonoBehaviour
     public string clipName;
     public Transform endPosition;
     private bool isActivated;
+    private bool isLev = false;
+    private Rigidbody rb;
     private void OnCollisionEnter(Collision collision)
     {
         if(isActivated)
         {
             SoundEffectManager.GlobalSFXManager.PlaySFX(clipName);
+            if(isLev)Destroy(rb);
             Destroy(this);
         }
     }
     public void Fly(float force)
     {
-        Rigidbody rb = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
         Vector3 direction = endPosition.position - transform.position;
         rb.AddForce(direction * force,ForceMode.Impulse);
         isActivated = true;
     }
     public void Levitate(float forceUp, float forceDown, float delay)
     {
-        Rigidbody rb = GetComponent<Rigidbody>();
+        isLev = true;
+        rb = GetComponent<Rigidbody>();
         rb.useGravity = false;
         rb.AddForce(Vector3.up * forceUp, ForceMode.Impulse);
         StartCoroutine(DropObject(delay, forceDown));
     }
     public IEnumerator DropObject(float delay,float forceDown)
     {
-        Rigidbody rb = GetComponent<Rigidbody>();
         yield return new WaitForSeconds(delay);
         isActivated = true;
         rb.useGravity = true;
         rb.AddForce(Vector3.down * forceDown, ForceMode.Impulse);
+    }
+    public void SetIsActivated(bool value)
+    {
+        isActivated = value;
     }
 
 }
