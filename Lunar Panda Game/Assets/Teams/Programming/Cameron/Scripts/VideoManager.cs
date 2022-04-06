bf089d85d20c83b32b59564d27196569bcf06273
Cerque <91639254+Cerque@users.noreply.github.com>
2022-04-06 02:48:05 +0100

    SkipOption for the cutscene
    Cameron Helped

--- Lunar Panda Game/Assets/Teams/Programming/Cameron/Scripts/VideoManager.cs
+++ Lunar Panda Game/Assets/Teams/Programming/Cameron/Scripts/VideoManager.cs
@@ -9,11 +9,41 @@ public class VideoManager : MonoBehaviour
     LevelManager levelManager;
     [SerializeField] string nextScene;
 
+    float time;
+    public float maxTime = 5f;
+    bool hasLoaded = false;
+
     void Start()
     {
         videoPlayer = GetComponent<VideoPlayer>();
         levelManager = FindObjectOfType<LevelManager>();
         videoPlayer.loopPointReached += LoadScene;
+    }
+
+    void Update()
+    {
+        Skip();
+    }
+
+    void Skip()
+    {
+        if (!hasLoaded)
+        {
+            if (Input.GetButton("Skip"))
+            {                
+                time += Time.deltaTime;
+                if (time > maxTime)
+                {
+                    hasLoaded = true;
+                    LoadScene(videoPlayer);
+                    Debug.Log("Skipped");
+                }
+            }
+            else if (Input.GetButtonUp("Skip"))
+            {
+                time = 0f;                
+            }
+        }
     }
 
     void LoadScene(VideoPlayer vp)
