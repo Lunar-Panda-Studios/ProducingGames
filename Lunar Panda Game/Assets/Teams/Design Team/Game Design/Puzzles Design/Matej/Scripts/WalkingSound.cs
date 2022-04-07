9448741b0674282ffc8df6c50a9a5a185301cc2d
Eoin Matthews <eoin95m@gmail.com>
2022-04-07 02:50:22 +0100

    Revert "GD Update"
    This reverts commit 792a2a2e383b92ff4f4304c84ceb7a29480911b1.

--- Lunar Panda Game/Assets/Teams/Design Team/Game Design/Puzzles Design/Matej/Scripts/WalkingSound.cs
+++ Lunar Panda Game/Assets/Teams/Design Team/Game Design/Puzzles Design/Matej/Scripts/WalkingSound.cs
@@ -25,46 +25,35 @@ public class WalkingSound : MonoBehaviour
     public SoundEffectManager soundEffectManager;
     void Start()
     {
-        //playing = true;
+        playing = true;
         p_rigidbody = gameObject.GetComponent<Rigidbody>();
     }
 
     void Update()
     {
-        if (canMakeSound && Time.timeScale > 0.1f)
+        if (canMakeSound && Time.timeScale > 0)
         {
-            Walking();
+            if (!soundSource.isActiveAndEnabled)
+                soundSource.enabled = true;
+            Vector2 velocityV2 = new Vector2(p_rigidbody.velocity.x, p_rigidbody.velocity.z);
+            float velocityMag = velocityV2.magnitude;
+            
+            if ((velocityMag > buffer || velocityMag < -buffer) && !playing && Time.timeScale > 0)
+            {
+                if (!soundSource.isPlaying)
+                    soundSource.Play();
+                soundSource.UnPause();
+                playing = true;
+            }
+            else if ((velocityMag > -buffer && velocityMag < buffer) && playing)
+            {
+                soundSource.Pause();
+                playing = false;
+            }
         }
         else
         {
             soundSource.enabled = false;
-            playing = false;
-        }
-    }
-
-    void Walking()
-    {
-        Vector2 velocityV2 = new Vector2(p_rigidbody.velocity.x, p_rigidbody.velocity.z);
-        float velocityMag = velocityV2.magnitude;
-
-        if (!soundSource.isActiveAndEnabled)
-        {
-            soundSource.enabled = true;
-        }
-
-        if ((velocityMag > buffer || velocityMag < -buffer) && !playing && Time.timeScale > 0)
-        {
-            if (!soundSource.isPlaying)
-            {
-                soundSource.Play();
-                soundSource.UnPause();
-                playing = true;
-            }
-        }
-
-        else if ((velocityMag > -buffer && velocityMag < buffer) && playing)
-        {
-            soundSource.Pause();
             playing = false;
         }
     }
